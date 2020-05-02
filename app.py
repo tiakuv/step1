1e9147cfc8eb13d31210e997fd6bab0d232f0938
tiakuv <tiakuv@gmail.com>
2020-05-03 01:17:10 +0300

    Normal commit 1

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
+from random import choices,choice,sample
+
 from flask import Flask, render_template, g
 from data import *
 
 app = Flask(__name__)
 
-@app.context_processor
-def dep_to_base():
-    return dict(departures=g.departures)
-
 @app.route('/')
 def main():
-    template_context = dict(title=title, subtitle=subtitle, description=description, tours=tours)
+    random_tours = sample(tours.items(), 6)
+    print(type(random_tours))
+    template_context = dict(title=title, subtitle=subtitle, description=description, tours=tours, departures=departures)
     output = render_template("index.html", **template_context)
     return output
 
 @app.route('/departures/<departure>')
 def show_dep(departure):
-    output = render_template("departure.html")
+
+    selected_tours= {}
+    for ind, tour in tours.items():
+        if tour['departure']==departure:
+            selected_tours[ind]=tour
+
+    output = render_template("departure.html", departures=departures, title=title, departure=departure, selected_tours=selected_tours)
     return output
 
 @app.route('/tours/<id>')
 def show_tour(id):
-    output = render_template("tour.html")
+    tour=tours[int(id)]
+    output = render_template("tour.html", departures=departures, title=title, tour=tour)
     return output
 
 app.run('localhost', 8000)
